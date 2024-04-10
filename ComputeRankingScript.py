52e1bdedef1379f7cd4a84e54fa281d339d193cd
Andrea Mastropietro <andrea.mastropietro@gmail.com>
2024-04-10 10:38:18 +0200

    Refactoring ComputeRankingsScript.py

--- ComputeRankingScript.py
+++ ComputeRankingScript.py
@@ -2,66 +2,52 @@ from src.GNNTrain import predict_from_saved_model
 from src.CreateDataset import get_dataset_from_graph
 from Paths import PATH_TO_GRAPHS, PATH_TO_RANKINGS
 from GDARanking import predict_candidate_genes
-import CreateDatasetv2_binary_diamond
+from src.CreateDataset import get_dataset_from_graph
 
 import os
 import sys
 from time import perf_counter
+import yaml
 
-# disease_Ids = ['C0006142',  'C0009402', 'C0023893', \
-#                'C0036341',  'C0376358', 'C3714756', \
-#                'C0860207',  'C0011581', 'C0005586', \
-#                'C0001973']
 
-disease_Ids = ['C0006142',  'C0009402', 'C0023893']
+available_methods = ['gnnexplainer', 'graphsvx',  'subgraphx']
 
-methods = ['gnnexplainer',  'gnnexplainer_only',\
-           'graphsvx',      'graphsvx_only',    \
-           'subgraphx',     'subgraphx_only']
+def check_args(METHODS, mode, num_cpus):
 
-def check_args(args):
-    if len(args) < 3:
-        if len(args) == 2 and (args[1] == '-h' or args[1] == '--help'):
-            print('\n\n[Usage]: python ComputeRankingScript.py disease_id explainability_method num_cores\n')
-            print('Available diseases:\n', disease_Ids, '\nType all to compute the ranking for all the available diseases\n')
-            print('Available methods:\n', methods, '\nType all to compute the ranking for all the available methods\n')
-            print('num_cores: define the number of cores to parallelize the explainability method\n\n')
-        else:
-            print('\n\n[ERR] Wrong input parameters: use -h or --help to print the usage\n\n')
-        return -1
 
-    disease_Id = args[1]
-    METHOD = args[2]
-    num_cpus = int(args[3])
-
-    if disease_Id not in disease_Ids and disease_Id.lower() != 'all':
-        print('\n[ERR] Disease ID', disease_Id, 'not present in the database\n')
-        return -1
-
-    if METHOD not in methods and METHOD.lower() != 'all':
-        print('\n[ERR] Method', METHOD, 'not available\n')
-        return -1
+    for METHOD in METHODS:
+        if METHOD not in available_methods:
+            print('\n[ERR] Method', METHOD, 'not available.\n')
+            sys.exit(-1)
     
+    if mode not in ['multiclass', 'binary']:
+        print('\n[ERR] Mode', mode, 'not available. Only binary and multiclass supported.\n')
+        sys.exit(-1)
+
     if num_cpus < 1:
-        print('\n[ERR]', num_cpus,'is an nvalid number of cores\n')
-        return -1
+        print('\n[ERR]', num_cpus,'is an nvalid number of cores.\n')
+        sys.exit(-1)
     
-    return disease_Id, METHOD, num_cpus
+    return None
 
-def ranking(disease_Id, METHOD, num_cpus, filename, modality='multiclass'):
+def ranking(disease_Id, METHOD, num_cpus, filename, modality='multiclass', from_diamond=False, model_name_prefix='GraphSAGE_', dataset_path=None, graph_path=None, save_ranking_path=None, quartiles=False):
 
-    model_name  = 'GraphSAGE_' + disease_Id + '_diamond'
-    graph_path  = PATH_TO_GRAPHS + 'grafo_diamond_nedbit_' + disease_Id + '.gml'
+    model_name  = model_name_prefix + disease_Id
+    if from_diamond:
+        model_name += '_diamond'
+
+    graph_path  = GRAPH_PATH + disease_Id + '.gml'
+
     classes     = ['P', 'LP', 'WN', 'LN', 'RN']
 
     if modality == 'binary':
         model_name += '_binary'
         classes = ['P', 'U']
-        dataset, G = CreateDatasetv2_binary_diamond.get_dataset_from_graph(graph_path, disease_Id, quartile=False)
-    else:
-        dataset, G = get_dataset_from_graph(graph_path, disease_Id, quartile=False, from_diamond=True)
 
-    model_name += '_40000_0_0005'
+
+    dataset, G = get_dataset_from_graph(graph_path, dataset_path, disease_Id, modality, quartiles, from_diamond=from_diamond)
+
+    #model_name += '_40000_0_0005' TO REMOVE AND RENAME THE MODEL
 
     preds, probs, model = predict_from_saved_model(model_name, dataset, classes, save_to_file=False)
 
@@ -92,57 +78,58 @@ def sanitized_input(prompt, accepted_values):
     return res
 
 if __name__ == '__main__':
+
+    args = None
+    
+    with open("parameters.yml") as paramFile:  
+        args = yaml.load(paramFile, Loader=yaml.FullLoader)
+
+    DISEASE_IDs = args["compute_ranking_script"]["DISEASE_IDs"]
+    EXPLAINERS = args["compute_ranking_script"]["EXPLAINERS"]
+    MODE = args["compute_ranking_script"]["MODE"]
+    NUM_CPUs = args["compute_ranking_script"]["NUM_CPUs"]
+    FROM_DIAMOND = args["compute_ranking_script"]["FROM_DIAMOND"]
+    GRAPH_PATH = args["compute_ranking_script"]["GRAPH_PATH"]
+    SAVE_RANKING_PATH = args["compute_ranking_script"]["SAVE_RANKING_PATH"]
+    MODEL_NAME_PREFIX = args["compute_ranking_script"]["MODEL_NAME_PREFIX"]
+
     t_start = perf_counter()
 
-    args = check_args(sys.argv)
-
-    if args == -1:
-        sys.exit()
+    check_args(EXPLAINERS, MODE, NUM_CPUs)
     
-    disease_Id = args[0]
-    METHOD = args[1]
-    num_cpus = args[2]
-
-    # modality = 'multiclass'
-    # if '_only' in METHOD:
-    #     modality = 'binary'
-
-    if disease_Id != 'all':
-        disease_Ids = [disease_Id]
+    print('[i] Computing the ranking for', DISEASE_IDs, '(', len(DISEASE_IDs), ')', 'disease(s).')
     
-    if METHOD != 'all':
-        methods = [METHOD]
-    
-    print('[i] Computing the ranking for', disease_Ids, '(', len(disease_Ids), ')', 'disease(s).')
-    
-    for disease_Id in disease_Ids:
+    for disease_Id in DISEASE_IDs:
 
-        for METHOD in methods:
+        for METHOD in EXPLAINERS:
             print('[i] Starting', disease_Id, 'with method', METHOD)
 
-            filename = PATH_TO_RANKINGS + disease_Id + '_all_positives_diamond_'
+            for mode in MODE:
+                print('[i] Starting', disease_Id, 'with method', METHOD, 'in mode', mode)
 
-            modality = 'multiclass'
-            if '_only' in METHOD:
-                modality = 'binary'
-            
-            if modality == 'multiclass':
-                filename += 'xgdag_' + METHOD.lower() + '.txt'
-            else:
-                filename += METHOD.lower().replace("_only", "") + '.txt'
+                filename = SAVE_RANKING_PATH + disease_Id # '_all_positives_diamond_'
+                if FROM_DIAMOND:
+                    filename += '_diamond_'
+                else:
+                    filename += '_'
+                
+                if MODE == 'multiclass':
+                    filename += 'xgdag_' + METHOD.lower() + '.txt'
+                else:
+                    filename += 'xgdag_' + METHOD.lower() + '_binary.txt'
 
-            res = ''
-            if os.path.exists(filename):
-                res = sanitized_input('[+] A raking for disease ' + disease_Id + \
-                    ' has already been computed with ' + METHOD + \
-                    '. Do you want to overwrite the old ranking? (y|n) ', ['y', 'n'])
-            
-            if res == 'n':
-                print('[i] Skipping disease', disease_Id, 'with method', METHOD)
-                continue
-            else:
-                # Compute the ranking
-                ranking(disease_Id, METHOD, num_cpus, filename, modality)
+                res = ''
+                if os.path.exists(filename):
+                    res = sanitized_input('[+] A raking for disease ' + disease_Id + \
+                        ' has already been computed with ' + METHOD + \
+                        '. Do you want to overwrite the old ranking? (y|n) ', ['y', 'n'])
+                
+                if res == 'n':
+                    print('[i] Skipping disease', disease_Id, 'with method', METHOD)
+                    continue
+                else:
+                    # Compute the ranking
+                    ranking(disease_Id, METHOD, NUM_CPUs, filename, mode, FROM_DIAMOND, MODEL_NAME_PREFIX)
 
     t_end = perf_counter()
     print('[i] Elapsed time:', round(t_end - t_start, 3))
